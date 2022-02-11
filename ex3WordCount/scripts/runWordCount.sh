934aef0a8cc945763a8e23a567a374b02caf8e26
dminahan <dfgriffith@gmail.com>
2022-02-11 01:36:18 -0500

    revert to fix permissions on script

--- ex3WordCount/scripts/runWordCount.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-if [ -z $1 ]; then
-  echo "Please provide input filename, Parameter 1 is empty"
-  exit 0
-else
-  java -cp ../target/ex3WordCount-0.0.1-SNAPSHOT.jar interview.exercises.wordcount.ExerciseWordCountMain $1
-fi
